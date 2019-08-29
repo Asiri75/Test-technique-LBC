3933996011023766a5de9f919716eb2a059e46eb
anas <asiri-anas@live.fr>
2019-08-29 11:13:15 +0200

    Created the model with titles and albums

---
+++ app/src/main/java/com/asiri/lbctest/model/Album.kt
@@ -0,0 +1,10 @@
+package com.asiri.lbctest.model
+
+/**
+ * Album class to group the data for albums list
+ */
+data class Album (
+    var id: Int,
+    var nbOfTitles: Int,
+    var thumbnailUrl: String
+)
---
+++ app/src/main/java/com/asiri/lbctest/model/Title.kt
@@ -0,0 +1,10 @@
+package com.asiri.lbctest.model
+
+
+data class Title (
+    var id: Int,
+    var albumId: Int,
+    var title: String,
+    var url: String,
+    var thumbnailUrl: String
+)
